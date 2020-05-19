8bf5679ddf1cdb1f5e1f4c51450c05ab89def444
raymondk <raymond@kth.se>
2020-05-19 22:25:25 +0200

    add day 05 part 2

--- src/util/day_05.rs
+++ src/util/day_05.rs
@@ -15,7 +15,7 @@ pub fn solve(input:String, part:Part) -> String {
 }
 
 fn part1(opcodes:Vec<i32>) -> String {
-    // 
+    //
     let mut inputs = vec![1];
     let mut outputs = vec![];
 
@@ -26,7 +26,13 @@ fn part1(opcodes:Vec<i32>) -> String {
 
 fn part2(opcodes:Vec<i32>) -> String {
     //
-    String::from("NA")
+    //
+    let mut inputs = vec![5];
+    let mut outputs = vec![];
+
+    let opcodes_out = int_codes(&mut inputs, &mut outputs, opcodes);
+
+    outputs.last().unwrap().to_string()
 }
 
 
@@ -113,7 +119,78 @@ fn int_codes(inputs:&mut Vec<i32>, outputs:&mut Vec<i32>, mut opcodes : Vec<i32>
             println!(" => OP 4: Push value {} to output.", output_value);
             // Advance program counter
             i+=2;
-        } else if opcode == 99  {
+        } else if opcode == 5 || opcode == 6 {
+            // Jump if true/false
+            let param1 = match is_pos_mode(instruction, 1) {
+                true => opcodes[opcodes[i+1] as usize],
+                false => opcodes[i+1],
+            };
+
+            let param2 = match is_pos_mode(instruction, 2) {
+                true => opcodes[opcodes[i+2] as usize],
+                false => opcodes[i+2],
+            };
+
+            let jump = if opcode == 5 {
+                if (param1 > 0) {
+                    println!(" => OP 5: {} > 0, set PC to {}", param1, param2);
+                    true
+                } else {
+                    println!(" => OP 5: {} !> 0", param1);
+                    false
+                }
+            } else if opcode == 6 {
+                if param1 == 0 {
+                    println!(" => OP 6: {} == 0, set PC to {}", param1, param2);
+                    true
+                } else {
+                    println!(" => OP 6: {} != 0", param1);
+                    false
+                }
+            } else { panic!("Uknown op..")};
+
+            if jump {
+                // Jump
+                i = param2 as usize;
+            } else {
+                i += 3;
+            }
+
+        } else if opcode == 7 || opcode == 8 {
+            // Jump if true/false
+            let param1 = match is_pos_mode(instruction, 1) {
+                true => opcodes[opcodes[i+1] as usize],
+                false => opcodes[i+1],
+            };
+
+            let param2 = match is_pos_mode(instruction, 2) {
+                true => opcodes[opcodes[i+2] as usize],
+                false => opcodes[i+2],
+            };
+
+            let res_index= opcodes[i+3] as usize;
+            let mut result = &mut opcodes[res_index];
+
+            if opcode == 7 && param1 < param2 {
+                println!(" => OP 7: {} < {}. Set 1 to index {}", param1, param2, res_index);
+                *result = 1;
+            } else if opcode == 7 {
+                println!(" => OP 7: {} !< {}. Set 0 to index {}", param1, param2, res_index);
+                *result = 0;
+            }
+
+            if opcode == 8 && param1 == param2 {
+                println!(" => OP 8: {} == {}. Set 1 to index {}", param1, param2, res_index);
+                *result = 1;
+            } else if opcode == 8 {
+                println!(" => OP 8: {} != {}. Set 0 to index {}", param1, param2, res_index);
+                *result = 0;
+            }
+
+            i += 4;
+        }
+
+        else if opcode == 99  {
             break;
         } else {
             panic!("Something went wrong!!")
@@ -198,5 +275,88 @@ mod tests {
 
     }
 
+    #[test]
+    fn test6() {
+        let opcodes= vec![3,9,8,9,10,9,4,9,99,-1,8];
+        let mut inputs = vec![9];
+        let mut outputs = vec![];
 
+        println!("{:?}", int_codes(&mut inputs, &mut outputs, opcodes));
+        println!("outputs = {:?}", outputs);
+    }
+
+    #[test]
+    fn test61() {
+        let opcodes= vec![3,9,7,9,10,9,4,9,99,-1,8];
+        let mut inputs = vec![7];
+        let mut outputs = vec![];
+
+        println!("{:?}", int_codes(&mut inputs, &mut outputs, opcodes));
+        println!("outputs = {:?}", outputs);
+    }
+
+    #[test]
+    fn test62() {
+        let opcodes= vec![3,3,1108,-1,8,3,4,3,99];
+        let mut inputs = vec![8];
+        let mut outputs = vec![];
+
+        println!("{:?}", int_codes(&mut inputs, &mut outputs, opcodes));
+        println!("outputs = {:?}", outputs);
+    }
+
+    #[test]
+    fn test63() {
+        let opcodes= vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                          1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+                          999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99];
+
+        let mut inputs = vec![8];
+        let mut outputs = vec![];
+
+        int_codes(&mut inputs, &mut outputs, opcodes);
+        assert_eq!(1000, *outputs.last().unwrap());
+
+    }
+
+    #[test]
+    fn test64() {
+        let opcodes= vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                          1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+                          999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99];
+
+        let mut inputs = vec![7];
+        let mut outputs = vec![];
+
+        int_codes(&mut inputs, &mut outputs, opcodes);
+        assert_eq!(999, *outputs.last().unwrap());
+
+
+    }
+
+
+    #[test]
+    fn test65() {
+        let opcodes= vec![3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                          1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+                          999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99];
+
+        let mut inputs = vec![10];
+        let mut outputs = vec![];
+
+        int_codes(&mut inputs, &mut outputs, opcodes);
+        assert_eq!(1001, *outputs.last().unwrap());
+    }
+
+    #[test]
+    fn test66() {
+        let opcodes = vec![3,225,1,225,6,6,1100,1,238,225,104,0,1101,72,36,225,1101,87,26,225,2,144,13,224,101,-1872,224,224,4,224,102,8,223,223,1001,224,2,224,1,223,224,223,1102,66,61,225,1102,25,49,224,101,-1225,224,224,4,224,1002,223,8,223,1001,224,5,224,1,223,224,223,1101,35,77,224,101,-112,224,224,4,224,102,8,223,223,1001,224,2,224,1,223,224,223,1002,195,30,224,1001,224,-2550,224,4,224,1002,223,8,223,1001,224,1,224,1,224,223,223,1102,30,44,225,1102,24,21,225,1,170,117,224,101,-46,224,224,4,224,1002,223,8,223,101,5,224,224,1,224,223,223,1102,63,26,225,102,74,114,224,1001,224,-3256,224,4,224,102,8,223,223,1001,224,3,224,1,224,223,223,1101,58,22,225,101,13,17,224,101,-100,224,224,4,224,1002,223,8,223,101,6,224,224,1,224,223,223,1101,85,18,225,1001,44,7,224,101,-68,224,224,4,224,102,8,223,223,1001,224,5,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,7,677,226,224,102,2,223,223,1005,224,329,101,1,223,223,8,677,226,224,1002,223,2,223,1005,224,344,1001,223,1,223,1107,677,677,224,102,2,223,223,1005,224,359,1001,223,1,223,1107,226,677,224,102,2,223,223,1005,224,374,101,1,223,223,7,226,677,224,102,2,223,223,1005,224,389,101,1,223,223,8,226,677,224,1002,223,2,223,1005,224,404,101,1,223,223,1008,226,677,224,1002,223,2,223,1005,224,419,1001,223,1,223,107,677,677,224,102,2,223,223,1005,224,434,101,1,223,223,1108,677,226,224,1002,223,2,223,1006,224,449,101,1,223,223,1108,677,677,224,102,2,223,223,1006,224,464,101,1,223,223,1007,677,226,224,102,2,223,223,1006,224,479,101,1,223,223,1008,226,226,224,102,2,223,223,1006,224,494,101,1,223,223,108,226,226,224,1002,223,2,223,1006,224,509,101,1,223,223,107,226,226,224,102,2,223,223,1006,224,524,101,1,223,223,1107,677,226,224,102,2,223,223,1005,224,539,1001,223,1,223,108,226,677,224,1002,223,2,223,1005,224,554,101,1,223,223,1007,226,226,224,102,2,223,223,1005,224,569,101,1,223,223,8,226,226,224,102,2,223,223,1006,224,584,101,1,223,223,1008,677,677,224,1002,223,2,223,1005,224,599,1001,223,1,223,107,226,677,224,1002,223,2,223,1005,224,614,1001,223,1,223,1108,226,677,224,102,2,223,223,1006,224,629,101,1,223,223,7,677,677,224,1002,223,2,223,1005,224,644,1001,223,1,223,108,677,677,224,102,2,223,223,1005,224,659,101,1,223,223,1007,677,677,224,102,2,223,223,1006,224,674,101,1,223,223,4,223,99,226];
+        let mut inputs = vec![5];
+        let mut outputs = vec![];
+
+        println!("{:?}", int_codes(&mut inputs, &mut outputs, opcodes));
+        println!("inputs = {:?}", inputs);
+        println!("outputs = {:?}", outputs);
+
+    }
 }
