dbb8a9e5be2f3d824b37ea9539860b85838665e7
raymondk <raymond@kth.se>
2020-05-19 21:38:18 +0200

    add day05 part 1

--- src/util.rs
+++ src/util.rs
@@ -2,6 +2,7 @@ mod day_01;
 mod day_02;
 mod day_03;
 mod day_04;
+mod day_05;
 
 pub enum Part {
     Part1,
@@ -16,6 +17,7 @@ pub fn get_solution(day:u8, part:Part, input:String) -> String {
         2 => day_02::solve(input, part),
         3 => day_03::solve(input, part),
         4 => day_04::solve(input, part),
+        5 => day_05::solve(input, part),
 
         _ => panic!("..."),
     }
